ab6a3ece294e40f052ce54be5cf91dc395e59380
EFNganze <113547855+EFNganze@users.noreply.github.com>
2022-10-09 16:15:57 +0000

    Create HOMEWORKS.py

---
+++ HOMEWORKS.py
@@ -0,0 +1,28 @@
+print('Hello World!')
+
+LAB 2.1
+a = int(input("write a number"))
+b = int(input("write another number"))
+if (a < b) :
+     print(a)
+elif (b < a) :
+      print(b)
+else :
+      print("the numbers are equal")
+    
+    LAB 2.2
+    
+    a = float(input("enter first number"))
+b = float(input("enter second number"))
+c = float(input("enter third number"))
+if (a >= b) and (a >= c) :
+    print(a)
+elif (b >= a) and (b >= c) :
+    print(b)
+else :
+    print(c)
+    
+    LAB 2.3
+    
+    
+       
