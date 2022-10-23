222112b15b945b29d5574c409afa7d427117ca6a
EFNganze <113547855+EFNganze@users.noreply.github.com>
2022-10-23 04:32:46 +0300

    Update correction assign.py

--- correction assign.py
+++ correction assign.py
@@ -4,7 +4,7 @@ rows = int(input("Enter number of rows: "))
 k = 0
 
 for i in range(1, rows+1):
-    for space in range(1, (rows-1)+1):
+    for space in range(1, (rows+1)-1):
         print(end=" ")
 
     while k!=(2*i-1):
